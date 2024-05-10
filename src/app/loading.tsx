aea42b3ef326a380c0ca9647d5d24d76ed344f64
denizyoldas <daksu30@gmail.com>
2024-05-11 00:32:58 +0300

    feat: skeleton component add

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,11 @@
-import LoadingSkeleton from '@components/UI/loading'
+import Skeleton from '@components/UI/skeleton'
 
 export default function Loading() {
-  return <LoadingSkeleton />
+  return (
+    <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-4">
+      {Array.from({ length: 4 }).map((_, i) => (
+        <Skeleton key={i} />
+      ))}
+    </div>
+  )
 }
